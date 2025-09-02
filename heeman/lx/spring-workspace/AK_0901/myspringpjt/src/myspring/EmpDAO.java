5bd8b0befd63e7609b5acb7a82383dd8629c6319
jjangkiyo <ox2120@naver.com>
2025-09-02 13:57:38 +0900

    empDAO를 spring+MyBatis식으로 바꿔서 풀기

--- heeman/lx/spring-workspace/AK_0901/myspringpjt/src/myspring/EmpDAO.java
+++ heeman/lx/spring-workspace/AK_0901/myspringpjt/src/myspring/EmpDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,54 +18,47 @@ public class EmpDAO {
 
 	@Autowired
 	DataSource dataSource;
+	@Autowired
+	SqlSession session;
 	
 	public List<Emp> getEmpListByDeptNo(int deptNo) throws Exception {
-		List<Emp> result = new ArrayList<Emp>();
-		// db 접속 데이터를 가져와서 result에 담는다.
-		// 커넥션을 생성한다.
-		Connection conn = dataSource.getConnection();
-		// String sql = "select * from employees where job_id = ?";
-
-		// Statement를 생성한다.
-		Statement stmt = conn.createStatement(); // Connection 인터페이스의 하위 클래스에 선언된 리턴타입이 Statement인 객체
-		// 쿼리를 실행하여 ResultSet을 참조한다.
-		String sql = "select * from employees where department_id = " + deptNo;
-		ResultSet rs = stmt.executeQuery(sql); // Statement 인터페이스에 선언된 리턴타입이 ResultSet인 객체
-
-		while (rs.next()) { // next 메서드 : ResultSet 인터페이스에 선언된 리턴타입이 ?
-			Emp emp = new Emp();
-			emp.id = rs.getInt("employee_id");
-			emp.lastName = rs.getString("last_Name");
-			emp.salary = rs.getInt("salary");
-
-			result.add(emp);
-
-		}
-		return result;
+		return session.selectList("getEmpListByDeptNo",deptNo);
+		/*		/*
+		 * List<Emp> result = new ArrayList<Emp>(); // db 접속 데이터를 가져와서 result에 담는다. //
+		 * 커넥션을 생성한다. Connection conn = dataSource.getConnection(); // String sql =
+		 * "select * from employees where job_id = ?";
+		 * 
+		 * // Statement를 생성한다. Statement stmt = conn.createStatement(); // Connection
+		 * 인터페이스의 하위 클래스에 선언된 리턴타입이 Statement인 객체 // 쿼리를 실행하여 ResultSet을 참조한다. String
+		 * sql = "select * from employees where department_id = " + deptNo; ResultSet rs
+		 * = stmt.executeQuery(sql); // Statement 인터페이스에 선언된 리턴타입이 ResultSet인 객체
+		 * 
+		 * while (rs.next()) { // next 메서드 : ResultSet 인터페이스에 선언된 리턴타입이 ? Emp emp = new
+		 * Emp(); emp.id = rs.getInt("employee_id"); emp.lastName =
+		 * rs.getString("last_Name"); emp.salary = rs.getInt("salary");
+		 * 
+		 * result.add(emp);
+		 * 
+		 * }
+		 */
 
 	}
 
 	public List<Emp> getEmpListByJobId(String jobId) throws Exception {
-
-		List<Emp> result = new ArrayList<Emp>();
-		// db 접속 데이터를 가져와서 result에 담는다.
-		// 커넥션을 생성한다.
-		Connection conn = dataSource.getConnection();
-		// Statement를 생성한다.
-		Statement stmt = conn.createStatement();
-		// 쿼리를 실행하여 ResultSet을 참조한다.
-		String sql = "select * from employees where job_id = ' " + jobId + " ' ";
-		ResultSet rs = stmt.executeQuery(sql);
-
-		while (rs.next()) {
-			Emp emp = new Emp();
-			emp.id = rs.getInt("employee_id");
-			emp.lastName = rs.getString("last_Name");
-			emp.salary = rs.getInt("salary");
-
-			result.add(emp);
-
-		}
-		return result;
+		return session.selectList("getEmpListByJobId",jobId);
+		/*
+		 * List<Emp> result = new ArrayList<Emp>(); // db 접속 데이터를 가져와서 result에 담는다. //
+		 * 커넥션을 생성한다. Connection conn = dataSource.getConnection(); // Statement를 생성한다.
+		 * Statement stmt = conn.createStatement(); // 쿼리를 실행하여 ResultSet을 참조한다. String
+		 * sql = "select * from employees where job_id = ' " + jobId + " ' "; ResultSet
+		 * rs = stmt.executeQuery(sql);
+		 * 
+		 * while (rs.next()) { Emp emp = new Emp(); emp.id = rs.getInt("employee_id");
+		 * emp.lastName = rs.getString("last_Name"); emp.salary = rs.getInt("salary");
+		 * 
+		 * result.add(emp);
+		 * 
+		 * }
+		 */
 	}
 }
